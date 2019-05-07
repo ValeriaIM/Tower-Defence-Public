6cbd5625c86f80ecacf246010957384e4a13a8ed
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2019-05-07 22:59:56 +0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -12,7 +12,7 @@ namespace Tower_Defence
         public int Damage { get; private set; }
         public int Value { get; private set; }
 
-        public Enemy(int hp, int damage, int speed, int value)
+        public Enemy(int hp, int damage, int value)
         {
             HP = hp;
             Damage = damage;
